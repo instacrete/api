f72d4ad9a7778c62749bf05e0d76f07fa2ee9410
Natalia Braz <naabraz@gmail.com>
2019-03-18 23:24:49 -0300

    Adicionando lint

--- src/routes/healthcheck/healthcheck.spec.js
+++ src/routes/healthcheck/healthcheck.spec.js
@@ -1,12 +1,12 @@
-const app = require('express')();
-const request = require('supertest');
+const app = require('express')()
+const request = require('supertest')
 
 const { healthcheck } = require('./healthcheck')
 
 describe('Healthcheck route', () => {
   test('It should response the healthcheck method', async () => {
     healthcheck(app)
-    const response = await request(app).get('/');
-    expect(response.statusCode).toBe(200);
+    const response = await request(app).get('/')
+    expect(response.statusCode).toBe(200)
   })
 })
